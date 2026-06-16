798aea53fc6ee20462bde976bfe270d498ab7ee6
Abdelrahman1Ab0A7med <ba246368@gmail.com>
2026-06-16 13:50:19 +0200

    Refactor in Doc class

--- ArchivAI.Application/DTOs/DocumentResponseDTO.cs
+++ ArchivAI.Application/DTOs/DocumentResponseDTO.cs
@@ -8,7 +8,7 @@ namespace ArchivAI.Application.DTOs
 {
     public class DocumentResponseDTO
     {
-        public Guid Id { get; set; }
+        //public Guid Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string OriginalFileName { get; set; } = string.Empty;
        // public string FilePath { get; set; } = string.Empty;
--- ArchivAI.Core/Common/BaseEntity.cs
+++ ArchivAI.Core/Common/BaseEntity.cs
@@ -6,7 +6,7 @@ namespace ArchivAI.Core.Common
 {
     public abstract class BaseEntity
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public DateTime? DeletedAt { get; set; }
